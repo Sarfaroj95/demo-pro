eafb10415c811fd779ca6148e5c1b8bd9e617d06
Sarfaroj <sarfaroj@qilinlab.com>
2020-11-25 12:52:35 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from "./app-routing.module"
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { HomeComponent } from "./home/home.component";
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { CareerComponent } from './career/career.component';
+import { JobComponent } from './job/job.component';
+import { JobtypeComponent } from './home/jobtype/jobtype.component';
+import { JobsComponent } from './job/jobs/jobs.component';
+import { RecentjobComponent } from './home/recentjob/recentjob.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SigninComponent,
+    SignupComponent,
+    CareerComponent,
+    JobComponent,
+    JobtypeComponent,
+    JobsComponent,
+    RecentjobComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
