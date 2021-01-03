da8c4ffce8d06b7e8c4d3c88a3479d0ec8133961
Sarfaroj <sarfaroj@qilinlab.com>
2021-01-03 23:19:35 +0530

    application form

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('demo-pro app is running!');
+    expect(page.getTitleText()).toEqual('Welcome to ng-mat3!');
   });
 
   afterEach(async () => {
