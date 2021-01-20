852e26623c974359ae7d5277d1a35b18c6b81313
Sarfaroj <sarfaroj@qilinlab.com>
2021-01-21 03:19:25 +0530

    update 2

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ng-mat3!');
+    expect(page.getTitleText()).toEqual('Welcome to demo-pro!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-mat3'`, () => {
+  it(`should have as title 'demo-pro'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-mat3');
+    expect(app.title).toEqual('demo-pro');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-mat3!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to demo-pro!');
   });
 });
